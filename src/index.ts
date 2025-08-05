6379d91b215deffea69d68da3a77e543d4a12296
Kauê Fraga Rodrigues <88486000+kauefraga@users.noreply.github.com>
2025-08-04 22:16:41 -0300

    feat: add ioredis and enqueue payments
    * Use `os.availableParallelism` and `cluster` module to make the server
      listen at port 3000 in the main process and spawn workers in worker
      processes

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
+import cluster from 'cluster';
 import { Elysia, t } from 'elysia';
+import Redis from 'ioredis';
+import { availableParallelism } from 'os';
 
 const paymentBody = t.Object({
-  correlationId: t.String(),
+  correlationId: t.String({ format: 'uuid' }),
   amount: t.Number(),
 });
 
@@ -10,11 +13,41 @@ const summaryQuery = t.Object({
   from: t.Optional(t.Date()),
 });
 
-const app = new Elysia()
-  .post("/payments", ({ body }) => body, { body: paymentBody })
-  .get("/payments-summary", ({ query }) => query, { query: summaryQuery })
-  .listen(3000);
+if (cluster.isPrimary) {
+  const numCPUs = availableParallelism();
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+  const redis = new Redis(process.env.REDIS_URL!);
+  console.log('redis connected');
+
+  const app = new Elysia()
+    .post("/payments", async ({ body }) => {
+      if (body.amount === 0) {
+        return // descarta rápido
+      }
+
+      const { correlationId, amount } = body;
+      const requestedAt = new Date();
+
+      await redis.lpush('payments', `${correlationId}:${amount}:${requestedAt}`);
+
+      return
+    }, { body: paymentBody })
+    .get("/payments-summary", ({ query }) => query, { query: summaryQuery })
+    .listen(3000);
+
+  console.log(
+    `🦊 #${cluster.worker?.id} elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  );
+} else {
+  // -- worker
+  // puxar mensagem da fila
+
+  // processar pagamento
+
+  // salvar no banco de dados
+
+  // confirmar processamento
+}
